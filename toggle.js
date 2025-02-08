d2c2a15f9706542b5866ad6709b0d042bd7e2889
Mohib Ali <alimohib025@gmail.com>
2025-02-08 17:34:14 +0500

    Done

--- toggle.js
+++ toggle.js
@@ -16,3 +16,18 @@ navItems.forEach(item => {
         item.classList.add("active-sec"); 
     };
 });
+
+document.addEventListener("scroll", () => {
+    const elements = document.querySelectorAll(".animate-on-scroll");
+
+    elements.forEach(element => {
+        const rect = element.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+
+        if (rect.top < windowHeight && rect.bottom > 0) {
+            element.classList.add("show"); 
+        } else {
+            element.classList.remove("show"); 
+        }
+    });
+});
