925e4e1b3e9aa16ef523e43ba4220a385d0d9d90
Mohib Ali <alimohib025@gmail.com>
2025-02-08 15:50:16 +0500

    Done all

--- toggle.js
+++ toggle.js
@@ -7,3 +7,12 @@ const closeBtn = document.getElementById("close");
 closeBtn.onclick = () => {
     navBar.classList.remove("active");
 };
+
+const navItems = document.querySelectorAll(".nav-item");
+
+navItems.forEach(item => {
+    item.onclick = () => {
+        document.querySelector(".active-sec")?.classList.remove("active-sec"); 
+        item.classList.add("active-sec"); 
+    };
+});
